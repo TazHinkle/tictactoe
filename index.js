3120f7b9a8b0e06a69b7e98d731432bcb4864380
Taz Hinkle <hinkle.taz@gmail.com>
2020-09-14 11:02:09 -0600

    Implemented gameOver state

--- index.js
+++ index.js
@@ -11,89 +11,87 @@ var changeCharInString = function(string, position, char) {
     return array.join('');
 };
 
+var playerWinMessage = function() {
+    winnerMessage.innerHTML = `
+        <p>Player has won!</p>
+        <p><a href="./index.html">Restart Game?</a></p>
+        `
+}
+
 var checkForXWin = function() {
     var array = gameStateString.split('');
     if(array[0] === 'x') {
         if(array[1]  === 'x' && array[2] === 'x') {
-            winnerMessage.innerHTML = `
-            <p>Player has won!</p>
-            `
+            playerWinMessage();
+            gameOver = true;
         } else if(array[3]  === 'x' && array[6] === 'x') {
-            winnerMessage.innerHTML = `
-            <p>Player has won!</p>
-            `
+            playerWinMessage();
+            gameOver = true;
         } else if(array[4]  === 'x' && array[8] === 'x') {
-            winnerMessage.innerHTML = `
-            <p>Player has won!</p>
-            `
+            playerWinMessage();
+            gameOver = true;
         }
     } else if(array[4] === 'x') {
         if(array[3]  === 'x' && array[5] === 'x') {
-            winnerMessage.innerHTML = `
-            <p>Player has won!</p>
-            `
+            playerWinMessage();
+            gameOver = true;
         } else if(array[1]  === 'x' && array[7] === 'x') {
-            winnerMessage.innerHTML = `
-            <p>Player has won!</p>
-            `
+            playerWinMessage();
+            gameOver = true;
         } else if(array[2]  === 'x' && array[6] === 'x') {
-            winnerMessage.innerHTML = `
-            <p>Player has won!</p>
-            `
+            playerWinMessage();
+            gameOver = true;
         }
     } else if(array[5] === 'x') {
         if(array[2]  === 'x' && array[8] === 'x') {
-            winnerMessage.innerHTML = `
-            <p>Player has won!</p>
-            `
+            playerWinMessage();
+            gameOver = true;
         }
     } else if(array[6] === 'x' && array[7] === 'x' && array[8] === 'x') {
-        winnerMessage.innerHTML = `
-            <p>Player has won!</p>
-            `
+        playerWinMessage();
+        gameOver = true;
     }
 };
+
+var playerLostMessage = function() {
+    winnerMessage.innerHTML = `
+            <p>Player has lost!</p>
+            <p><a href="./index.html">Restart Game?</a></p>
+            `
+}
 
 var checkForOWin = function() {
     var array = gameStateString.split('');
     if(array[0] === 'o') {
         if(array[1]  === 'o' && array[2] === 'o') {
-            winnerMessage.innerHTML = `
-            <p>Player has lost!</p>
-            `
+            playerLostMessage();
+            gameOver = true;
         } else if(array[3]  === 'o' && array[6] === 'o') {
-            winnerMessage.innerHTML = `
-            <p>Player has lost!</p>
-            `
+            playerLostMessage();
+            gameOver = true;
         } else if(array[4]  === 'o' && array[8] === 'o') {
-            winnerMessage.innerHTML = `
-            <p>Player has lost!</p>
-            `
+            playerLostMessage();
+            gameOver = true;
         }
     } else if(array[4] === 'o') {
         if(array[3]  === 'o' && array[5] === 'o') {
-            winnerMessage.innerHTML = `
-            <p>Player has lost!</p>
-            `
+            playerLostMessage();
+            gameOver = true;
         } else if(array[1]  === 'o' && array[7] === 'o') {
-            winnerMessage.innerHTML = `
-            <p>Player has lost!</p>
-            `
+            playerLostMessage();
+            gameOver = true;
         } else if(array[2]  === 'o' && array[6] === 'o') {
-            winnerMessage.innerHTML = `
-            <p>Player has lost!</p>
-            `
+            playerLostMessage();
+            gameOver = true;
         }
     } else if(array[5] === 'o') {
         if(array[2]  === 'o' && array[8] === 'o') {
-            winnerMessage.innerHTML = `
-            <p>Player has lost!</p>
-            `
+            playerLostMessage();
+            gameOver = true;
         }
     } else if(array[6] === 'o' && array[7] === 'o' && array[8] === 'o') {
-        winnerMessage.innerHTML = `
-            <p>Player has lost!</p>
-            `
+        playerLostMessage();
+        gameOver = true;
     }
 }
 
@@ -145,24 +143,53 @@ var renderGameCell = function(i) {
     return cell;
 }
 
+var renderGameOverCell = function(i) {
+    cell = `
+        |
+        ${gameStateString[i]}
+        |
+    `
+    return cell;
+}
+
 var renderGameBoard = function() {
-    gameBoard.innerHTML = `
-        <p>
-            ${renderGameCell(0)}
-            ${renderGameCell(1)}
-            ${renderGameCell(2)}
-        </p>
-        <p>
-            ${renderGameCell(3)}
-            ${renderGameCell(4)}
-            ${renderGameCell(5)}
-        </p>
-        <p>
-            ${renderGameCell(6)}
-            ${renderGameCell(7)}
-            ${renderGameCell(8)}
-        </p>
+    if(!gameOver) {
+        gameBoard.innerHTML = `
+            <p>
+                ${renderGameCell(0)}
+                ${renderGameCell(1)}
+                ${renderGameCell(2)}
+            </p>
+            <p>
+                ${renderGameCell(3)}
+                ${renderGameCell(4)}
+                ${renderGameCell(5)}
+            </p>
+            <p>
+                ${renderGameCell(6)}
+                ${renderGameCell(7)}
+                ${renderGameCell(8)}
+            </p>
             `
+    } else {
+        gameBoard.innerHTML = `
+            <p>
+                ${renderGameOverCell(0)}
+                ${renderGameOverCell(1)}
+                ${renderGameOverCell(2)}
+            </p>
+            <p>
+                ${renderGameOverCell(3)}
+                ${renderGameOverCell(4)}
+                ${renderGameOverCell(5)}
+            </p>
+            <p>
+                ${renderGameOverCell(6)}
+                ${renderGameOverCell(7)}
+                ${renderGameOverCell(8)}
+            </p>
+            `
+    }
 }
 
 actUponHashParameters(window.location.href);
